5773775b45e01eb10eac977805196ff0201b15e8
beautifulbanana <18943107757@163.com>
2018-08-08 21:06:29 +0800

    芒3改

--- lucenemm/src/main/java/com/demo/BeautifulMei.java
+++ lucenemm/src/main/java/com/demo/BeautifulMei.java
@@ -11,5 +11,15 @@ public class BeautifulMei {
 	public void setAge(String age) {
 		this.age = age;
 	}
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
