6cd6e59bae12176fe5f927f27a6973ef19b3b8e1
beautifulbanana <18943107757@163.com>
2018-08-08 20:59:33 +0800

    芒2上

---
+++ lucenemm/src/main/java/com/demo/BeautifulMei.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+public class BeautifulMei {
+
+	private String age;
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+}
